a338247b6a932a5513a8ec1322137e76c2c03452
kanan <116582983+kanan42@users.noreply.github.com>
2022-11-08 18:38:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,55 @@
-// write your code here
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n > 0 && n <= 1000) {
+		if  (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19 || n == 21 || n == 22 || n == 23 || n == 29 || n == 31) {
+			cout << n << " number is prime";
+		}
+	}
+	if (n % 2 == 0) {
+		cout << "2 ";
+	}
+	 if (n % 3 == 0) {
+		cout << "3 ";
+	}
+	 if (n % 5 == 0) {
+		cout << "5 ";
+	}
+	 if (n % 7 == 0) {
+		cout << "7 ";
+	}
+
+	else if (n % 9==0) {
+		cout << "9 ";
+	}
+	else if (n % 11 == 0){
+		cout << "11 ";
+	}
+	 if (n % 13 == 0) {
+		cout << "13 ";
+	}
+	 if (n % 17 == 0) {
+		cout << "17 ";
+	}
+	 if (n % 19 == 0) {
+		cout << "19 ";
+	}
+	else if (n % 21 == 0) {
+		cout << "21 ";
+	}
+	else if (n % 23==0) {
+		cout << "23 ";
+	}
+	else if (n % 29 == 0) {
+		cout << "29 ";
+	}
+	else if (n % 31 == 0) {
+		cout << "31 ";
+	}
+	return 0;
+
+}
 
